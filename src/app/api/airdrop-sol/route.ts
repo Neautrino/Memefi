d34a638fc6c4c29311252eb9208259ea5b54c862
Subhendu Singh <ssubhendu988@gmail.com>
2025-02-03 15:04:21 +0000

    Request airdrop

---
+++ src/app/api/airdrop-sol/route.ts
@@ -0,0 +1,37 @@
+import {
+	Connection,
+	LAMPORTS_PER_SOL,
+	clusterApiUrl,
+	PublicKey,
+} from "@solana/web3.js";
+import { NextRequest } from "next/server";
+const connection = new Connection(clusterApiUrl("devnet"));
+
+export async function POST(request: NextRequest) {
+	const body = await request.json();
+
+	try {
+		const airdropSignature = await connection.requestAirdrop(
+			new PublicKey(body.publicKey),
+			body.amount * LAMPORTS_PER_SOL
+		);
+
+		const { value } = await connection.getSignatureStatuses([airdropSignature]);
+
+		if(value === null) {
+			throw new Error('Airdrop failed');
+		}
+	
+		console.log(value);
+	
+		return new Response(JSON.stringify({ value }), {
+			headers: { 'Content-Type': 'application/json' },
+		});
+	} catch (error: any) {
+		return new Response(JSON.stringify({ error: error.message }), {
+			headers: { 'Content-Type': 'application/json' },
+		});
+	}
+
+    
+}
